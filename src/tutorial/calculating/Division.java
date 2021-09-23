460e48a6ead600d4c0da8fbd75d3c6a5badad59d
Aquizi <47073178+Aquizi@users.noreply.github.com>
2021-09-22 22:59:51 -0400

    SecondsInDay.java

---
+++ src/tutorial/calculating/Division.java
@@ -0,0 +1,7 @@
+package tutorial.calculating;
+
+public class Division {
+    public static void main(String[] args){
+
+    }
+}
--- src/tutorial/variables/ReadingVariables.java
+++ src/tutorial/variables/ReadingVariables.java
@@ -7,9 +7,22 @@ public class ReadingVariables {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write a value: ");
-        int value = Integer.parseInt(scanner.nextLine());
-        System.out.println("You wrote " + value);
+        System.out.println("Enter some text: ");
+        String text = scanner.nextLine();
 
+        System.out.print("Enter a whole number: ");
+        int wholeNumber = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter a decimal number: ");
+        double decimalNumber = Float.parseFloat(scanner.nextLine());
+
+        System.out.print("Enter true or false: ");
+        boolean trueFalse = Boolean.parseBoolean(scanner.nextLine());
+
+        System.out.println();
+        System.out.println("String: " + text);
+        System.out.println("int: " + wholeNumber);
+        System.out.println("double: " + decimalNumber);
+        System.out.println("Boolean: " + trueFalse);
     }
 }
